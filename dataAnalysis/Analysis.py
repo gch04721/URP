ba869c09a4c5fb48765915b7fba5dafcc06d1b33
gch04721 <jwhuh@uclab.re.kr>
2019-10-18 16:34:21 +0900

    plot

--- dataAnalysis/Analysis.py
+++ dataAnalysis/Analysis.py
@@ -1,6 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+with open('edgeDataQueue.txt', 'r') as f:
+    data = f.readlines()
+    raw = np.zeros((5, 3600))
+    count = 0
+    for val in data:
+        raw[count % 5][int(count / 5)] = float(val)
+        count += 1
+
+    for i in range(0,5):
+        plt.plot(range(0,3600), raw[i])
+        plt.show()
+    
+
 with open('nodeDataQueue.txt', 'r') as f:
     data = f.readlines()
     raw = np.zeros((15, 3600))
@@ -9,6 +22,8 @@ with open('nodeDataQueue.txt', 'r') as f:
         raw[count%15][int(count/15)] = float(val)
         count += 1
     
+    plt.xlabel('time slot')
+    plt.ylabel('node dataqueue, 1 = 100Kbit')
     plt.plot(range(0, 3600), raw[3])
     plt.show()
 
